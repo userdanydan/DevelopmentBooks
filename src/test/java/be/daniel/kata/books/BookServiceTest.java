56102d6c0d1175f31ee2dd46965bbe436b798187
daniel <daniel.imeri@zetes.com>
2023-04-30 11:34:41 +0200

    Setting test for Book service.

---
+++ src/test/java/be/daniel/kata/books/BookServiceTest.java
@@ -0,0 +1,18 @@
+package be.daniel.kata.books;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class BookServiceTest {
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @InjectMocks
+    private BookServiceImpl bookService;
+
+}
