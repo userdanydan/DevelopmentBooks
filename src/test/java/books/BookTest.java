27ff91c561ddf317fb77fa314ffa9e0fe01b663f
daniel <daniel.imeri@zetes.com>
2023-04-30 09:50:51 +0200

    Created test for Book class.

--- src/test/java/books/BookTest.java
+++ src/test/java/books/BookTest.java
@@ -1,4 +1,15 @@
 package books;
 
-public class BookTest {
+import org.junit.jupiter.api.Test;
+
+class BookTest {
+
+    @Test
+    void aBookHasATittle(){
+        Book book = new Book();
+        var expected = "testTitle";
+        book.setTitle(expected);
+        var actual = book.getTitle();
+        assetEquals(expected, actual);
+    }
 }
